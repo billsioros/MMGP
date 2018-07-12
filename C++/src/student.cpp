0ac3fb36b23d28a491e9ee26603fffc8b708b710
billsioros <billsioros97@gmail.com>
2018-07-12 19:01:04 +0300

    Clustering 1.10

--- C++/src/student.cpp
+++ C++/src/student.cpp
@@ -4,8 +4,22 @@
 #include <bitset>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 std::ostream& operator<<(std::ostream& os, const Student& student)
 {
-    os << "| " << student._position << " | " << student._timespan << " | " << student._days << " |";
+    os << "| " << student._studentId << " | " << student._position << " | " << student._timespan << " | " << student._days << " |";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bus& bus)
+{
+    os << "***" << std::endl;
+    os << '|' << std::right << std::setw(3) << std::setfill('0') << bus._number;
+    os << '|' << std::right << std::setw(3) << std::setfill('0') << bus._capacity;
+    os << '|' << std::right << std::setw(3) << std::setfill('0') << bus._students.size() << std::endl;
+    os << "***" << std::endl;
+
+    if (!bus._students.empty())
+        for (const auto& student : bus._students)
+            os << student << std::endl;
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. Threading
 
 #### TAGS:
-1. Clustering 1.10
+1. Clustering 1.11
 
 ## LOGGING: (./bin/MMGP >> out.txt) 2>&1 | tee -a log.txt
 
