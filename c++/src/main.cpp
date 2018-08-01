f0f1079e4337384700baadaec9e6d8cf4a6e4012
billsioros <billsioros97@gmail.com>
2018-08-01 12:24:17 +0300

    createSchedules1 & 2

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -4,20 +4,38 @@
 #include "cluster.hpp"
 #include "manager.hpp"
 #include <list>
-#include <vector>
 #include <iostream>
-#include <ctime>
 #include <memory>
 #include <limits>
 #include <stdexcept>
 #include <cmath>
+#include <unordered_map>
+#include <chrono>
+
+#include <queue>
+#include <unordered_set>
+#include <stack>
+
+using Group     = Cluster<Manager::Student>;
+using Buses     = std::vector<Manager::Bus>;
+using Schedules = std::list<Buses>;
 
 double _evaluation(
     SQLite::Database&,
-    const Cluster<Manager::Student>&,
-    const Cluster<Manager::Student>&,
+    const Group&,
+    const Group&,
     const std::string&,
     double w0, double w1);
+
+void createSchedules1(
+    const Group *,
+    const Buses&,
+    Schedules&);
+
+void createSchedules2(
+    const Group *,
+    const Buses&,
+    Schedules&);
 
 int main(int argc, char * argv[])
 {
@@ -51,46 +69,30 @@ int main(int argc, char * argv[])
     std::list<Manager::Student> students;
     Manager::load(*database, students, argv[2]); // Manager::print(students);
 
-    std::clock_t beg = std::clock();
+    auto beg = std::chrono::high_resolution_clock().now();
 
-    const Cluster<Manager::Student> * cluster =
-        Cluster<Manager::Student>::hierarchical(students,
-            [&](const Cluster<Manager::Student>& A, const Cluster<Manager::Student>& B)
+    const Group * group =
+        Group::hierarchical(students,
+            [&](const Group& A, const Group& B)
             {
                 return _evaluation(*database, A, B, argv[2], w0, w1);
             });
 
-    std::cout << "\n Elapsed time: " << (std::clock() - beg) / (double) CLOCKS_PER_SEC << std::endl;
+    auto end = std::chrono::high_resolution_clock().now();
 
-    students.clear();
+    std::chrono::duration<double, std::micro> elapsed = end - beg;
+    std::cerr << "<MSG>: Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 
-    std::vector<Manager::Bus> buses;
+    Buses buses;
     Manager::load(*database, buses); // Manager::print(buses);
 
-    std::list<std::vector<Manager::Bus>> schedules; schedules.push_back(buses);
-    
-    std::size_t busId = 0UL;
-    cluster->traverse(
-        [&](const Cluster<Manager::Student>& cluster)
-        {
-            if (busId >= buses.size())
-            {
-                schedules.push_back(buses); busId = 0UL;
-            }
+    Schedules schedules;
+    createSchedules1(group, buses, schedules);
+    // createSchedules2(group, buses, schedules);
 
-            if (schedules.back()[busId]._students.size() < schedules.back()[busId]._capacity)
-                schedules.back()[busId]._students.push_back(cluster.centroid());
-            else
-                busId++;
-        });
+    delete group;
 
-    delete cluster;
-
-    for (const auto& schedule : schedules)
-    {
-        Manager::json(schedule);
-        // Manager::csv(schedule);
-    }
+    Manager::json(argv[2], schedules);
 
     return 0;
 }
@@ -120,8 +122,8 @@ static double intersection(const Vector2& A, const Vector2& B)
 
 double _evaluation(
     SQLite::Database& database,
-    const Cluster<Manager::Student>& A,
-    const Cluster<Manager::Student>& B,
+    const Group& A,
+    const Group& B,
     const std::string& daypart,
     double w0, double w1)
 {
@@ -129,9 +131,9 @@ double _evaluation(
     const Manager::Student * best[]   = { nullptr, nullptr };
     double min[]                      = { -1.0,       -1.0 };
 
-    auto nearest = [&](const Cluster<Manager::Student>& cluster)
+    auto nearest = [&](const Group& group)
     {
-        const Manager::Student * cen = &cluster.centroid();
+        const Manager::Student * cen = &group.centroid();
         
         double distance;
         if ((distance = haversine(cen->_position, target[0]->_position)) < min[0])
@@ -165,4 +167,156 @@ double _evaluation(
     dt = intersection(A.centroid()._timespan, B.centroid()._timespan);
 
     return (1.0 - w1) * (1.0 / dx) + w1 * dt;
+}
+
+void createSchedules1(
+    const Group * group,
+    const Buses& buses,
+    Schedules& schedules)
+{
+    std::unordered_map<const Group *, std::size_t> visited;
+
+    auto remaining = [&](const Group * target)
+    {
+        if (visited.find(target) == visited.end())
+            visited[target] = 0UL;
+
+        return target->size() - visited[target];
+    };
+
+    std::function<void(const Group *, Manager::Bus&)> traverse =
+    [&](const Group * target, Manager::Bus& bus)
+    {
+        if (target->left() && target->right())
+        {
+            const std::size_t lrem = remaining(target->left());
+            const std::size_t rrem = remaining(target->right());
+            
+            #ifdef __DEBUG_SCHEDULE__
+            std::cout << "<DBG>: remaining(target->left()): "  << lrem << std::endl;
+            std::cout << "<DBG>: remaining(target->right()): " << rrem << std::endl;
+            #endif
+
+            std::pair<const Group *, std::size_t> brem, srem;
+
+            if (lrem > rrem)
+            {
+                brem.first = target->left();  brem.second = lrem;
+                srem.first = target->right(); srem.second = rrem;
+            }
+            else
+            {
+                srem.first = target->left();  srem.second = lrem;
+                brem.first = target->right(); brem.second = rrem;
+            }
+
+            if (brem.second && bus._students.size() <= bus._capacity)
+            {
+                const std::size_t pvisited = visited[brem.first];
+
+                traverse(brem.first, bus); visited[target] += visited[brem.first] - pvisited;
+            }
+
+            if (srem.second && bus._students.size() <= bus._capacity)
+            {
+                const std::size_t pvisited = visited[srem.first];
+
+                traverse(srem.first, bus); visited[target] += visited[srem.first] - pvisited;
+            }
+        }
+        else
+        {
+            if (remaining(target) && bus._students.size() <= bus._capacity)
+            {
+                visited[target]++; bus._students.push_back(target->centroid());
+            }
+        }
+    };
+
+    std::size_t busId = std::numeric_limits<std::size_t>().max();
+    while (remaining(group))
+    {
+        if (busId >= buses.size())
+        {
+            schedules.push_back(buses); busId = 0UL;
+        }
+
+        #ifdef __DEBUG_SCHEDULE__
+        std::cout << "<DBG>: remaining(group): " << remaining(group);
+        std::cout << "<DBG>: busId: " << busId << std::endl;
+        std::cout << "<DBG>: schedules.size(): " << schedules.size() << std::endl;
+        std::cout << std::endl;
+        #endif
+
+        Manager::Bus& bus = schedules.back()[busId]; traverse(group, bus);
+
+        if (bus._students.size() >= bus._capacity)
+            busId++;
+    }
+}
+
+void createSchedules2(
+    const Group * group,
+    const Buses& buses,
+    Schedules& schedules)
+{
+    std::unordered_set<const Group *> visited;
+
+    auto assign = [&visited](const Group * group, Manager::Bus& bus)
+    {
+        std::stack<const Group *> adjacent; adjacent.push(group);
+
+        do
+        {
+            const Group * current = adjacent.top(); adjacent.pop();
+
+            if (visited.find(current) == visited.end())
+                visited.insert(current);
+            else
+                continue;
+
+            if (current->left() && current->right())
+            {
+                adjacent.push(current->left());
+                adjacent.push(current->right());
+            }
+            else
+            {
+                bus._students.push_back(current->centroid());
+            }
+
+        } while (!adjacent.empty());
+    };
+
+    std::size_t busId = std::numeric_limits<std::size_t>().max();
+
+    std::queue<const Group *> adjacent; adjacent.push(group);
+    do
+    {
+        const Group * current = adjacent.front(); adjacent.pop();
+
+        if (current->size() <= schedules.back()[busId]._capacity)
+        {
+            if (busId >= buses.size())
+            {
+                schedules.push_back(buses); busId = 0UL;
+            }
+
+            assign(current, schedules.back()[busId]);
+
+            busId++;
+        }
+
+        if (visited.find(current) == visited.end())
+            visited.insert(current);
+        else
+            continue;
+
+        if (current->left() && current->right())
+        {
+            adjacent.push(current->left());
+            adjacent.push(current->right());
+        }
+
+    } while (!adjacent.empty());
 }
